05a76de2d7f51161b5740263f5c024e640f146d8
masilvax <jakub.bartek@gmail.com>
2026-05-11 17:44:24 +0200

    db.json

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
