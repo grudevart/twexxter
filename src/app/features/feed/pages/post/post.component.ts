113da36a3469535762bd70c19e37a88e6f0ee36f
Kozłowski <artur.kozlowski@capgemini.com>
2026-05-28 14:26:24 +0200

    post list

--- src/app/features/feed/pages/post/post.component.ts
+++ src/app/features/feed/pages/post/post.component.ts
@@ -1,6 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { Post } from '../../models/post.model';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post',
@@ -9,4 +11,22 @@ import {MatCardModule} from '@angular/material/card';
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
-export class PostComponent {}
+export class PostComponent {
+  
+@Input() post!: Post;
+
+  constructor(private postService: PostService) {}
+
+
+  // likePost(): void {
+  //   const updatedPost: Post = {
+  //     ...this.post,
+  //     likes: this.post.likes + 1
+  //   };
+
+  //   this.postService.updatePost(updatedPost).subscribe(res => {
+  //     this.post = res; // update UI
+  //   });
+  // }
+
+}
